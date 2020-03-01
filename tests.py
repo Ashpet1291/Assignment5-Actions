d8be4abbe8dfd79b09fee510db9e019db1a62406
Ashpet1291 <49249566+Ashpet1291@users.noreply.github.com>
2020-03-01 13:20:13 -0800

    fixed line issues for _name_ and _main_

--- tests.py
+++ tests.py
@@ -2,15 +2,16 @@ import unittest
 import task
 
 
-class TestCase (unittest.TestCase) :
+class TestCase(unittest.TestCase):
+    def test1(self):
+        expected = "success"
+        self.assertEqual(expected, task.firstrun())
+        
+    def test2(self):
+        expected = "failure"
+        self.assertNotEqual(expected, task.firstrun())
+        
 
-  def test1(self) :
-    expected = "success"
-    self.assertEqual(expected, task.firstrun())
-    
-    def test2(self) :
-      expected = "failure"
-      self.assertNotEqual(expected, task.firstrun())
-      
-if _name_ == '_main_':
-  unittest.main
+
+if __name__ == '__main__':
+    unittest.main()
