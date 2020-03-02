00590943f6734d8bf004ec97b4c64bcbfba5192c
Ashpet1291 <49249566+Ashpet1291@users.noreply.github.com>
2020-03-01 16:14:46 -0800

    Update tests.py

--- tests.py
+++ tests.py
@@ -5,11 +5,11 @@ import task
 class TestCase(unittest.TestCase):
     def test1(self):
         expected = "success"
-        self.assertEqual (expected, task.firstrun())
+        self.assertEqual(expected, task.firstrun())
 
     def test2(self):
         expected = "failure"
-        self.assertNotEqual (expected, task.firstrun())
+        self.assertNotEqual(expected, task.firstrun())
 
 
 if __name__ == '__main__':
