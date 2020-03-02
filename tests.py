c02218d32ec2bc785f8fe89c3cf6a363f4f65999
Ashpet1291 <49249566+Ashpet1291@users.noreply.github.com>
2020-03-01 23:33:08 -0800

    testfunction daysInBetween

--- tests.py
+++ tests.py
@@ -25,6 +25,13 @@ class TestList(TestCase):
     def test_first_n_last_list(self):
         testLists = [1, 2, 3, 4, 5, 6]
         self.assertEqual('[1, 6]', task.firstNLastList(testLists))
+        
+        
+class TestDaysInBetween(TestCase):
+    def test_days_in_between(self):
+        dateA = date(2014, 7, 2)
+        dateB = date(2014, 7, 11)
+        self.assertEqual(9, task.daysInBetween(dateA, dateB))
 
 
 if __name__ == '__main__':
