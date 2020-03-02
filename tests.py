8893bb36d7140f196622d8211bc0b6e5b30f0079
Ashpet1291 <49249566+Ashpet1291@users.noreply.github.com>
2020-03-01 17:37:19 -0800

    areatest

--- tests.py
+++ tests.py
@@ -11,6 +11,13 @@ class TestCase(unittest.TestCase):
         expected = "failure"
         self.assertNotEqual(expected, task.firstrun())
 
+class TestArea(TestCase):
+    def test_circle_area(self):
+        rad = 5
+        self.assertEqual(78.5, task.circleArea(rad))
+
+        rad = 7
+        self.assertEqual(153.86, task.circleArea(rad))
 
 if __name__ == '__main__':
     unittest.main()
