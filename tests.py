6d2c44c4a23fa22a7abb22adddeecdc3968e9204
Ashpet1291 <49249566+Ashpet1291@users.noreply.github.com>
2020-03-01 17:49:31 -0800

    Update tests.py

--- tests.py
+++ tests.py
@@ -19,7 +19,7 @@ class TestArea(TestCase):
 
         rad = 7
         self.assertEqual(153.86, task.circleArea(rad))
-        
+
         rad = 2
         self.assertEqual(12.56, task.circle(rad))
 
